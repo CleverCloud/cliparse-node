1df3e92056a9737728330613ba916c90c524a936
Clement Delafargue <clement@delafargue.name>
2015-01-17 03:08:00 +0100

    Cleaned up the example

--- example.js
+++ example.js
@@ -3,32 +3,48 @@
 var optparse = require("./optparse.js");
 var parsers = require("./parsers.js");
 
+var echoModule = function(v) {
+  if(v.options.reverse === true) {
+    console.log(v.args[0].split("").reverse().join(""));
+  } else {
+    console.log(v.args[0]);
+  }
+};
+
+var addModule = function(v) {
+  console.log(v.args[0] + 2);
+};
+
+
+
+
 var testCli = optparse.cli({
   name: "testCli",
   commands: [
-    optparse.command("echo", {
-      description: "display the given value",
-      args: [ optparse.argument("value", {
-        helpT: "simple value"
-      })],
-      options: [ optparse.flag("reverse") ]
-    }, function(v) {
-        if(v.options.reverse === true) {
-          console.log(v.args[0].split("").reverse().join(""));
-        } else {
-          console.log(v.args[0]);
-        }
-    }),
-    optparse.command("add2", {
-      description: "add 2 to the given integer and display the result",
-      args: [ optparse.argument("int", {
-        defaultValue: 0,
-        parser: parsers.intParser,
-        helpT: "int to add 2 to"
-      })]
-    }, function(v) { console.log(v.args[0] + 2); })
+
+    optparse.command(
+      "echo",
+      { description: "display the given value",
+        args: [ optparse.argument("value", { helpT: "simple value" })],
+        options: [ optparse.flag("reverse") ]
+      },
+      echoModule),
+
+    optparse.command(
+      "add2",
+      { description: "add 2 to the given integer and display the result",
+        args: [
+          optparse.argument("int",
+            { defaultValue: 0,
+              parser: parsers.intParser,
+              helpT: "int to add 2 to" })
+        ]
+      },
+      addModule)
   ]
 });
+
+
 
 optparse.parseValues(testCli);
 
