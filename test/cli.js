160544c52f0b069d61fcc4f3a6d3fec0d907ebbd
Willem van den Ende <willem@qwan.eu>
2016-06-28 17:00:24 +0100

    Parse arguments on ubuntu linux
    This should fix https://github.com/divarvel/cliparse-node/issues/18

--- test/cli.js
+++ test/cli.js
@@ -3,11 +3,13 @@ var test = require("tape");
 var cli = require("../src/cli.js");
 
 test('clean argument values', function(t) {
-    t.plan(4);
+    t.plan(5);
 
     t.same(cli.cleanArgv([ "node test.js" ]), []);
     t.same(cli.cleanArgv([ "node.exe", "test.js", "arg1" ]), [ "arg1"]);
     t.same(cli.cleanArgv([ "C:\\path\\to\\node.exe", "test.js", "arg1" ]), [ "arg1"]);
+    t.same(cli.cleanArgv([ '/usr/bin/nodejs','test.js', 'arg1']), ['arg1' ]);
+
     t.same(cli.cleanArgv([ "my-executable", "arg1" ]), [ "arg1"]);
 });
 
